367db6759fbb80e7d474026a5dd5e9c7a19c5d9d
anvarturakulov <anvarsam@mail.ru>
2020-05-21 14:48:28 +0500

    added review

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ $(function(){
       dots: true,
       arrows:true,
     });
+
+    $(".review__slider").slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true,
+      arrows:false,
+    });
 });
 
 
